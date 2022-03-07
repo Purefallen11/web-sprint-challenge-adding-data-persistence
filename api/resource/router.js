07d010cb3233df777aa9b51cf36b4d95ac33f68d
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-03-06 22:22:06 -0600

    doing resource model and router, doing 1 by 1 while checking them with postman

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,23 @@
-// build your `/api/resources` router here
+// - [ ] `[POST] /api/resources`
+//   - Example of response body: `{"resource_id":1,"resource_name":"foo","resource_description":null}`
+
+// - [ ] `[GET] /api/resources`
+//   - Example of response body: `[{"resource_id":1,"resource_name":"foo","resource_description":null}]`
+const express = require('express')
+
+
+const Resource = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Resource.findResource()
+        .then(resource => {
+        res.status(200).json(resource)
+        })
+        .catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-
+const resourceRouter = require('./resource/router')
 const server = express()
 
 server.use(express.json())
-
+server.use('/api/resources', resourceRouter)
 module.exports = server
