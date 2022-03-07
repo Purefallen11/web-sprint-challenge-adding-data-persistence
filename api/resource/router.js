3ec1e318609f1c14b5201582f8a3517c8b70b6d3
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-03-06 22:33:20 -0600

    first get endpoint on resources works, working on the post now

--- api/resource/router.js
+++ api/resource/router.js
@@ -15,9 +15,13 @@ router.get('/', (req, res, next) => {
         .then(resource => {
         res.status(200).json(resource)
         })
-        .catch(err){
-        res.status(500).json({message: err.message})
-    }
+        .catch(next)
+})
+
+router.use((err, req, res,) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
 })
 
 module.exports = router
